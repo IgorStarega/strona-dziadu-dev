512f6eba4b0d6867f0840bde0639dbc71aac2859
Wilson <staregaigor@gmail.com>
2025-11-06 15:53:29 +0100

    opa

--- desktopy/Na kartkowke/zmienne2.js
+++
@@ -1,6 +0,0 @@
-var imie = prompt("Podaj imię: ")
-var nazwisko = prompt("Podaj nazwisko: ")
-
-document.write('<a style="color:blue">Imie, </a> ' + '<a>' + imie + '</a>')
-document.write('<br>')
-document.write('<a style="color:red">Nazwisko, </a> ' + '<a>' + nazwisko + '</a>')
